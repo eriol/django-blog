6473fb8bf161796cc3e84b583d0fcfe95848967d
Daniele Tricoli <eriol@mornie.org>
2009-10-25 16:19:58 +0100

    Added an enhanced version of object_detail to show also entries with DRAFT or HIDDEN status only if user is in the staff group

--- blog/urls.py
+++ blog/urls.py
@@ -8,6 +8,7 @@ entry_info = {
     'queryset': Entry.live.all(),
     'date_field': 'pub_date',
 }
+
 entry_info_month = dict(entry_info, month_format='%m')
 
 category_info = {
@@ -25,10 +26,13 @@ urlpatterns = patterns('django.views.generic.date_based',
      'blog_entry_archive_month'),
     (r'^(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/$', 'archive_day',
      entry_info_month, 'blog_entry_archive_day'),
-    (r'^(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/(?P<slug>[-\w]+)/$',
-     'object_detail', entry_info_month, 'blog_entry_detail'),
-
 )
+
+urlpatterns += patterns('',
+    (r'^(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/(?P<slug>[-\w]+)/$',
+     'blog.views.enhanced_object_detail', entry_info_month, 'blog_entry_detail'),
+)
+
 
 urlpatterns += patterns('django.views.generic.list_detail',
     (r'^category/(?P<slug>[-\w]+)/$', 'object_detail', category_info,
--- blog/views.py
+++ blog/views.py
@@ -1,1 +1,21 @@
-# Create your views here.
+# -*- coding: utf-8 -*-
+from django.views.generic.date_based import object_detail
+
+from blog.models import Entry
+
+def enhanced_object_detail(*args, **kwargs):
+    '''Show entries with DRAFT or HIDDEN status if user.is_staff is True'''
+
+    request = args[0]
+
+    if request.user.is_staff:
+        entry_info = {
+            'queryset': Entry.objects.all(),
+            'date_field': 'pub_date',
+            'month_format': '%m',
+        }
+
+        kwargs.update(entry_info)
+        return object_detail(*args, **kwargs)
+
+    return object_detail(*args, **kwargs)
