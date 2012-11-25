33a07751d18773c539e667bb949ff6c775da0f10
Daniele Tricoli <eriol@mornie.org>
2012-11-25 17:57:44 +0100

    Enhanced version handling

--- blog/__init__.py
+++ blog/__init__.py
@@ -1,2 +1,6 @@
 # -*- coding: utf-8 -*-
-VERSION = '0.0.4'
+VERSION = (0, 0, 4)
+
+def get_version():
+    """Returns project version in a human readable form."""
+    return '.'.join(str(v) for v in VERSION)
--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
 
-from blog import VERSION
+from blog import get_version
 
 setup(
     name='blog',
-    version=VERSION,
+    version=get_version(),
     description='A simple blog app for Django.',
     author='Daniele Tricoli',
     author_email='eriol@mornie.org',
