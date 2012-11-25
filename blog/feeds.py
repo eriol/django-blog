72e05f7d0aac07f50b4eaa18e47a9c949841319f
Daniele Tricoli <eriol@mornie.org>
2012-11-25 16:30:56 +0100

    Fixed import

--- blog/feeds.py
+++ blog/feeds.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from django.contrib.sites.models import Site
-from django.contrib.syndication.feeds import Feed, FeedDoesNotExist
+from django.contrib.syndication.views import Feed, FeedDoesNotExist
 from django.core.exceptions import ObjectDoesNotExist
 from django.core.urlresolvers import reverse
 from django.utils.feedgenerator import Atom1Feed
